5c97979403fd81e72f75d9e4ac1c6e9f2357e536
Tegzy-cmd <77752563+Tegzy-cmd@users.noreply.github.com>
2023-11-27 21:11:42 +0100

    worked on app.js and html page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@ import { Weather } from "./weather.js";
 import { User } from "./user.js";
 import { Fact } from "./fact.js";
 let Username = document.getElementById("username");
-let Country = document.getElementById("countryName");
+let location = document.getElementById("Location");
 let temp = document.getElementById("temp");
 let city = document.getElementById("city");
 let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
 let weatherIcon = document.getElementById("weatherIcon");
-const searchBtn = document.querySelector(".search button");
+let Locate = document.getElementById("location");
 let countryName = document.getElementById("cName")
 let capital = document.getElementById("capt")
 let region = document.getElementById("reg")
@@ -20,16 +20,17 @@ let form = document.querySelector("form")
 form.addEventListener("submit", (e) => {
   e.preventDefault()
   let userName = Username.value;
-  let country = Country.value;
+  let country = location.value;
   checkWeather(userName, country);
   form.reset()
 });
 
 async function checkWeather(username, country) {
   const user = new User(username, country);
-  const weather = new Weather(user.country);
+  const weather = new Weather(country);
   await weather.getWeather();
-checkFact(weather.iso)
+  Locate.innerHTML = weather.city
+  checkFact(weather.city)
   temp.innerHTML = weather.temperature + "°C";
   city.innerHTML = weather.city;
   humidity.innerHTML = weather.humidity + "%";
